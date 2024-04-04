cd7aa1edccaa31bc1bc521dc61a283af902e0a7f
Georg Schramm <georg.schramm@kuleuven.be>
2024-04-04 08:29:20 +0200

    clean up

--- python/main.py
+++ python/main.py
@@ -2,8 +2,7 @@
 
 from __future__ import annotations
 
-import array_api_strict as xp
-import array_api_compat.numpy as np
+import numpy as np
 import matplotlib.pyplot as plt
 
 from collections.abc import Callable
@@ -11,10 +10,10 @@ from array_api_strict._array_object import Array
 
 
 def exp_pdf(x: Array) -> Array:
-    if xp.min(x) >= 0:
-        p = xp.exp(-x)
+    if np.min(x) >= 0:
+        p = np.exp(-x)
     else:
-        p = xp.where(x >= 0, xp.exp(-x), xp.zeros_like(x))
+        p = np.where(x >= 0, np.exp(-x), np.zeros_like(x))
     return p
 
 
@@ -26,37 +25,31 @@ shifted_pdf: Callable[[Array, float], Array] = lambda t, delta: pdf(t - delta)
 eta: Callable[[Array, float], Array] = lambda t, delta: pdf(t - delta) / pdf(t) - 1
 
 # %%
-t = xp.linspace(0, 50, 10000, endpoint=False)
+t = np.linspace(0, 50, 10000, endpoint=False)
 dt = float(t[1] - t[0])
 p = pdf(t)
 
 num_deltas = 16
-num_sim = 2000
-bb = xp.zeros(num_sim)
+num_sim = 100
+bb = np.zeros(num_sim)
 
 np.random.seed(1)
 
 for i_sim in range(num_sim):
-    deltas = xp.sort(xp.asarray(10 * np.random.rand(num_deltas)))
-    U = xp.zeros((num_deltas, num_deltas))
+    deltas = np.sort(10 * np.random.rand(num_deltas))
+    U = np.zeros((num_deltas, num_deltas))
 
     for i in range(num_deltas):
         eta_i = eta(t, float(deltas[i]))
         tmp = eta_i * p
         for j in range(num_deltas):
             eta_j = eta(t, float(deltas[j]))
-            U[i, j] = xp.sum(eta_j * tmp) * dt
+            U[i, j] = np.sum(eta_j * tmp) * dt
             U[j, i] = U[i, j]
 
-    pinv_U = xp.linalg.pinv(U, rtol=1e-6)
+    pinv_U = np.linalg.pinv(U, rcond=1e-6)
 
-    bb[i_sim] = float(xp.sum(deltas * (pinv_U @ deltas)))
-    print(i_sim, bb[i_sim])
+    bb[i_sim] = float(np.sum(deltas * (pinv_U @ deltas)))
+    print(f"{i_sim:04}/{num_sim:04}, {bb[i_sim]:.2E}", end="\r")
 
-# fig, ax = plt.subplots(2, 2, figsize=(8, 8), tight_layout=True)
-# for i, axx in enumerate(ax.ravel()):
-#    d = 0.2 * i + 0.5
-#    axx.plot(np.asarray(t), np.asarray(pdf(t)))
-#    axx.plot(np.asarray(t), np.asarray(shifted_pdf(t, d)))
-#    axx.plot(np.asarray(t), np.asarray(eta(t, d)) * pdf(t))
-# fig.show()
+print()
